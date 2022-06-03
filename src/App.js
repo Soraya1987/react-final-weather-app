8a926a90afa30be787c7c3d032ac7cff47e6fe6e
Soraya <soraya_mahmoudi95@yahoo.com>
2022-06-03 22:22:17 +0430

    Added the GitHub repo url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather App
+     <h1>Weather App</h1>
+     <footer>
+       This project was coded by Soraya Mahmoudi and is {" "}
+       <a href="https://github.com/Soraya1987/react-final-weather-app" target="_blank">open-sourced on GitHub</a>
+     </footer>
     </div>
   );
 }
